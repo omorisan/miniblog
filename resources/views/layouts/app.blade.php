edff775f78f3935c48f94637a4faf6dfbe359db9
omorisan <2022@omorisan.jp>
2024-03-27 16:11:25 +0900

    トップページ編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Denryu.site</title>
     <link rel="stylesheet" href="/main.css">
 </head>
 <body>
     <header>
-        <a href="/" class="site-title">ミニブログ</a>
+        <a href="/" class="site-title">Denryu.site</a>
         <nav class="tab">
+            <div>こんちゃ！</div>
+            {{---
             <ul>
                 @if (Auth::check())
                 <li><a class="tab-item{{ Request::is('home') ? ' active' : ''}}" href="{{ route('home') }}">マイページ</a></li>
@@ -27,7 +29,7 @@
                 <li><a href="{{ route('login') }}">ログイン</a></li>
                 
                 @endif
-            </ul>
+            </ul>--}}
         </nav>
     </header>
     <main class="container">
